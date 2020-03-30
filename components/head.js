f75ce18ac608a1fa441fa2335ed333eb53c6e24b
M Haidar Hanif <git@mhaidarhanif.com>
2020-03-31 03:09:19 +0800

    Update favicons path

--- components/head.js
+++ components/head.js
@@ -16,32 +16,20 @@ const Head = props => (
     />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
 
-    <link
-      rel='apple-touch-icon'
-      sizes='180x180'
-      href='/static/apple-touch-icon.png'
-    />
+    <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
     <link
       rel='icon'
       type='image/png'
       sizes='32x32'
-      href='/static/favicon-32x32.png'
+      href='/favicon-32x32.png'
+      s
     />
-    <link
-      rel='icon'
-      type='image/png'
-      sizes='16x16'
-      href='/static/favicon-16x16.png'
-    />
-    <link rel='manifest' href='/static/manifest.json' />
-    <link
-      rel='mask-icon'
-      href='/static/safari-pinned-tab.svg'
-      color='#ffdd33'
-    />
+    <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+    <link rel='manifest' href='/manifest.json' />
+    <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#ffdd33' />
     <meta name='msapplication-TileColor' content='#ffdd33' />
     <meta name='theme-color' content='#ffffff' />
-    <link rel='icon' href='/static/favicon.ico' />
+    <link rel='icon' href='/favicon.ico' />
 
     <meta property='og:url' content={props.url || defaultOGURL} />
     <meta property='og:title' content={props.title || ''} />
